7199a69884db834e36bb338b16e13ec14caef1d7
unsubble <unsubble@gmail.com>
2026-09-17 16:43:17 +0300

    [ADD] imlement api features such as Manager and ServiceFactory.

---
+++ src/main/java/com/unsubble/smokin/api/ClientManager.java
@@ -0,0 +1,60 @@
+package com.unsubble.smokin.api;
+
+import com.unsubble.smokin.executor.ClientService;
+import com.unsubble.smokin.model.Protocol;
+import com.unsubble.smokin.model.RequestGroup;
+import com.unsubble.smokin.model.Version;
+import com.unsubble.smokin.parser.HttpRequestParser;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class ClientManager {
+
+    private static volatile ClientManager manager;
+    private static final Object LOCK = new Object();
+    private final HttpRequestParser parser;
+    private final ClientServiceFactory clientServiceFactory;
+
+    private ClientManager() {
+        parser = new HttpRequestParser();
+        clientServiceFactory = new ClientServiceFactory();
+    }
+
+    public HttpRequestParser getParser() {
+        return parser;
+    }
+
+    public ClientService getDefaultService(Protocol protocol, String host, int port) {
+        return clientServiceFactory.create(protocol, host, port);
+    }
+
+    public ClientService getService(Version version, Protocol protocol, String host, int port) {
+        return clientServiceFactory.create(version, protocol, host, port);
+    }
+
+    public static ClientManager getInstance() {
+        ClientManager instance = manager;
+
+        if (instance == null) {
+            throw new IllegalStateException("ClientManager is not initialized yet");
+        }
+
+        return instance;
+    }
+
+    public static void initialize() {
+        if (manager != null) {
+            throw new IllegalStateException("ClientManager is already initialized");
+        }
+
+        synchronized (LOCK) {
+            if (manager != null) {
+                throw new IllegalStateException("ClientManager is already initialized");
+            }
+
+            manager = new ClientManager();
+        }
+    }
+}
---
+++ src/main/java/com/unsubble/smokin/api/ClientServiceFactory.java
@@ -0,0 +1,69 @@
+package com.unsubble.smokin.api;
+
+import com.unsubble.smokin.encoder.Http1RequestEncoder;
+import com.unsubble.smokin.encoder.HttpRequestEncoder;
+import com.unsubble.smokin.executor.ClientService;
+import com.unsubble.smokin.executor.HttpClient;
+import com.unsubble.smokin.model.Protocol;
+import com.unsubble.smokin.model.Version;
+import com.unsubble.smokin.parser.*;
+import com.unsubble.smokin.transport.TcpTransport;
+import com.unsubble.smokin.transport.TlsTransport;
+import com.unsubble.smokin.transport.Transport;
+
+import java.util.Objects;
+
+public class ClientServiceFactory {
+
+    ClientServiceFactory() {
+    }
+
+    ClientService create(Protocol protocol, String host, int port) {
+        Objects.requireNonNull(protocol);
+        Objects.requireNonNull(host);
+
+        return new ClientService(() -> createClient(Version.UNKNOWN, protocol, host, port));
+    }
+
+    ClientService create(Version version, Protocol protocol, String host, int port) {
+        Objects.requireNonNull(version);
+        Objects.requireNonNull(protocol);
+        Objects.requireNonNull(host);
+
+        return new ClientService(() -> createClient(version, protocol, host, port));
+    }
+
+    private HttpClient createClient(Version version, Protocol protocol, String host, int port) {
+        HttpRequestEncoder requestEncoder = createRequestEncoder(version);
+        HttpResponseParser responseParser = createResponseParser(version);
+        Transport transport = createTransport(protocol, host, port);
+        HttpResponseFramer responseFramer = createResponseFramer(version, transport);
+
+        return new HttpClient(requestEncoder, responseParser, responseFramer, transport);
+    }
+
+    private Transport createTransport(Protocol protocol, String host, int port) {
+        return switch (protocol) {
+            case HTTP -> new TcpTransport(host, port);
+            case HTTPS -> new TlsTransport(host, port);
+        };
+    }
+
+    private HttpResponseFramer createResponseFramer(Version version, Transport transport) {
+        return switch (version) {
+            case UNKNOWN, HTTP_1_1 -> new Http1ResponseFramer(transport);
+        };
+    }
+
+    private HttpRequestEncoder createRequestEncoder(Version version) {
+        return switch (version) {
+            case UNKNOWN, HTTP_1_1 -> new Http1RequestEncoder();
+        };
+    }
+
+    private HttpResponseParser createResponseParser(Version version) {
+        return switch (version) {
+            case UNKNOWN, HTTP_1_1 -> new Http1ResponseParser();
+        };
+    }
+}
